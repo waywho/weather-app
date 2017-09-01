e88aafd179b0b075538b88cdcf8e92c9c12d02ec
waywho <walzerfan@yahoo.com>
2017-09-01 20:32:11 +0200

    add icons

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 require 'sinatra'
 require 'config_env'
 require_relative 'models/weather.rb'
+require 'uri'
 
 ConfigEnv.init("#{__dir__}/config/env.rb")
 
 set :port, 3000
 
 get '/' do
-	weather = Weather.new
+	@cities = [
+		"San Francisco",
+		"London",
+		"Tokyo",
+		"Moscow",
+		"Cairo"
+	]
+	@city = params[:city] || "San Francisco"
+	weather = Weather.new(@city)
 	@temp = weather.temp
+	@icons = weather.icons
 	erb :index
 end
